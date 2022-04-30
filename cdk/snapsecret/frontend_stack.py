1de8846dc49f19234cde1cf8a9c30bb981eca13b
Greg Cockburn <gergnz@gmail.com>
2022-04-30 11:21:38 +1000

    make the npm buld as per https://github.com/aws/aws-cdk/issues/8707

--- cdk/snapsecret/frontend_stack.py
+++ cdk/snapsecret/frontend_stack.py
@@ -125,7 +125,13 @@ class FrontendStack(Stack):
         )
 
         # Deploy the frontend assets to S3 and invalidate the CloudFront Cache
-        build_command = f'npm install && VITE_WEBAPI_ENDPOINT="{apigw_url}" npm run build && cp -au dist/* /asset-output'
+        setup_command = "&&".join([
+            "export npm_config_update_notifier=false",
+            "export npm_config_cache=$(mktemp -d)",
+            "npm install",
+        ])
+        build_command = f'VITE_WEBAPI_ENDPOINT="{apigw_url}" npm run build && cp -au dist/* /asset-output'
+        complete_build_command = setup_command + "&&" + build_command
         s3_deployment.BucketDeployment(
             self,
             id="snapsecret_frontend_deploy",
@@ -142,8 +148,8 @@ class FrontendStack(Stack):
                         image=lambda_.Runtime.NODEJS_14_X.bundling_image,
                         command=[
                             "bash",
-                            "-c",
-                            build_command,
+                            "-xc",
+                            complete_build_command,
                         ],
                     ),
                 )
